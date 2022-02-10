5977ce7744b655dfb4d3cef6717d3e29870c94d3
KH.Lee <opallios@gmail.com>
2022-02-10 22:16:46 +0900

    07 chapter beautify movie app

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
         ) : (
           <div className="movies">
             {movies.map((movie) => (
-              <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+              <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
             ))}
           </div>
         )}
