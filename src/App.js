5d340a0b8e4bdc41b581c0ccc9204e39db0d4411
KH.Lee <opallios@gmail.com>
2022-02-14 19:37:38 +0900

    08-3 make navigation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     // react-router-dom이 v6로 업그레이드 되면서 Route를 Routes로 감싸줘야 함.
     // Component path params 또한 component -> element로 사용하며 Value도 component 태그를 사용해야 함.
-
     <HashRouter>
+      <Navigation />
       <Routes>
         <Route path="/" exact={true} element={<Home />} />
         <Route path="/about" element={<About />} />
